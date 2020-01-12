faf4b5c8b9d687e1aea032d5fc6ae85604fc90c0
weffelms <stacie.weffelmeyer@wwt.com>
2020-01-11 21:33:09 -0600

    minor fixes

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -17,7 +17,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     payment_method_types: ['card'],
     // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`, // `http://localhost:3000/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     success_url: `${req.protocol}://${req.get('host')}/my-tours`,
-    cancel_url:  `${req.protocol}://${req.get('host')}/tour/${tour.slug}`, // `http://localhost:3000/tour/${tour.slug}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`, // `http://localhost:3000/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
     line_items: [
@@ -25,7 +25,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         name: `${tour.name} Tour`,
         description: tour.summary,
         images: [
-          `https://www.natours.dev/img/tours/${tour.imageCover}` // `http://localhost:3000/img/tours/${tour.imageCover}` // `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}` // `https://www.natours.dev/img/tours/${tour.imageCover}`
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}` // `https://www.natours.dev/img/tours/${tour.imageCover}` // `http://localhost:3000/img/tours/${tour.imageCover}` // `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}` // `https://www.natours.dev/img/tours/${tour.imageCover}`
         ],
         amount: tour.price * 100,
         currency: 'usd',
@@ -55,7 +55,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async session => {
   const tour = session.client_reference_id
   const user = (await User.findOne({ email: session.customer_email })).id
-  const price = session.line_items[0].amount / 100
+  const price = session.display_items[0].amount / 100
   await Booking.create({ tour, user, price })
 }
 
